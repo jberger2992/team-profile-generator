7c81902c0a4d94f8852fe2ed8f0319b8226d7062
jberger2992 <jberger2992@gmail.com>
2023-04-25 20:50:51 -0400

    finished

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Team Profile Generator
+## Description
+Gives prompts in oder to generates a webpage that displays my team's basic info.
+[Video Demonstration](https://watch.screencastify.com/v/1Cin4okCN1MjOfUpuLDm)
+## Installation
+N/A
+## Usage
+N/A
--- index.js
+++ index.js
@@ -8,78 +8,6 @@ const fs = require("fs");
 
 const teamMembers = [];
 
-// const newMember = ()=>{
-//     let empInfo = ""
-//     inquirer.prompt([
-//         {
-//             type:"input",
-//             message:"What is the team member's name?",
-//             name:"name"
-//         },
-//         {
-//             type:"input",
-//             message:"What is the team member's ID?",
-//             name:"id"
-//         },
-//         {
-//             type:"input",
-//             message:"What is the team member's email address?",
-//             name:"email"
-//         },
-//         {
-//             type:"list",
-//             message:"What is the team member's role?",
-//             name:"role",
-//             choices:["Manager","Engineer","Intern"]
-//         },
-//     ]).then(ans=>{
-//         console.log(ans);
-//         empInfo = ans;
-//         console.log(empInfo);
-//         if(ans.role === "Manager"){
-//             inquirer.prompt([
-//                 {
-//                     type:"input",
-//                     message:"What is this manager's office number?",
-//                     name:"officeNum"
-//                 }
-//             ]).then(num=>{
-//                 const newManager = new Manager(empInfo.name,empInfo.id,empInfo.email,num.officeNum);
-//                 console.log(newManager);
-//                 teamMembers.push(newManager);
-//                 newOrDone()
-//             })
-//         }
-//         if(ans.role === "Engineer"){
-//             inquirer.prompt([
-//                 {
-//                     type:"input",
-//                     message:"What is this engineer's office GitHub?",
-//                     name:"github"
-//                 }
-//             ]).then(git=>{
-//                 const newEngineer = new Engineer(empInfo.name,empInfo.id,empInfo.email,git.github);
-//                 console.log(newEngineer);
-//                 teamMembers.push(newEngineer);
-//                 newOrDone()
-//             })
-//         }
-//         if(ans.role === "Intern"){
-//             inquirer.prompt([
-//                 {
-//                     type:"input",
-//                     message:"What is this intern's school?",
-//                     name:"school"
-//                 }
-//             ]).then(sch=>{
-//                 const newIntern = new Intern(empInfo.name,empInfo.id,empInfo.email,sch.school);
-//                 console.log(newIntern);
-//                 teamMembers.push(newIntern);
-//                 newOrDone()
-//             })
-//         }
-//     })
-// }
 
 const newOrDone = ()=>{
     inquirer.prompt([
@@ -92,17 +20,17 @@ const newOrDone = ()=>{
     ]).then(ans=>{
         console.log(ans);
         if(ans.answer === "New Engineer"){
-            newMember();
+            newEngineer();
         }
         if(ans.answer === "New Intern"){
-            newMember();
+            newIntern();
         }
         if(ans.answer === "Finished"){
             const htmlString = generateTeam(teamMembers);
             fs.writeFile('output/index.html', htmlString, (err) =>
-      err ? console.log(err) : console.log('Successfully created index.html!')
+            err ? console.log(err) : console.log('Successfully created index.html!')
     );
-        }
+}
     })
 }
 
@@ -155,7 +83,7 @@ const newEngineer = ()=>{
         },
         {
             type:"input",
-            message:"What is this Engineer's office GitHub?",
+            message:"What is this Engineer's GitHub?",
             name:"github"
         }
     ]).then(ans=>{
@@ -189,11 +117,11 @@ const newIntern = ()=>{
             name:"school"
         }
     ]).then(ans=>{
-                const newIntern = new Intern(ans.name,ans.id,ans.email,ans.school);
-                console.log(newIntern);
-                teamMembers.push(newIntern);
-                newOrDone()
+        const newIntern = new Intern(ans.name,ans.id,ans.email,ans.school);
+        console.log(newIntern);
+        teamMembers.push(newIntern);
+        newOrDone()
     })
 }
 
-newMember();
+newManager();
